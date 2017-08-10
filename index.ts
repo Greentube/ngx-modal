70b5c9f5b274ade8d0d73881773f1449682e506b
Miroslav Jonas <meeroslav@yahoo.com>
2017-08-10 15:40:43 +0200

    Fix ComponentFactoryResolver for lazy routes in ng4

--- index.ts
+++ index.ts
@@ -3,27 +3,28 @@ export * from './src/modal-dialog.service';
 export * from './src/modal-dialog.component';
 export * from './src/modal-dialog.interface';
 
-import { NgModule, ModuleWithProviders } from '@angular/core';
-
 // components and directives
 import { ModalDialogComponent } from './src/modal-dialog.component';
 import { ModalDialogService } from './src/modal-dialog.service';
 import { SimpleModalComponent } from './src/simple-modal.component';
+import { ModalDialogInstanceService } from './src/modal-dialog-instance.service';
 // modules
 import { CommonModule } from '@angular/common';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 
 @NgModule({
-    imports: [ CommonModule ],
-    declarations: [ ModalDialogComponent, SimpleModalComponent ],
-    entryComponents: [ ModalDialogComponent, SimpleModalComponent ],
-    exports: [ ModalDialogComponent, SimpleModalComponent ]
+  imports: [CommonModule],
+  declarations: [ModalDialogComponent, SimpleModalComponent],
+  entryComponents: [ModalDialogComponent, SimpleModalComponent],
+  exports: [ModalDialogComponent, SimpleModalComponent],
+  providers: [ModalDialogService]
 })
 export class ModalDialogModule {
 
-    static forRoot(): ModuleWithProviders {
-        return {
-            ngModule: ModalDialogModule,
-            providers: [ ModalDialogService ]
-        };
-    }
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: ModalDialogModule,
+      providers: [ModalDialogInstanceService]
+    };
+  }
 }
